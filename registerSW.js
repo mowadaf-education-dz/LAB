6d621d4cd54f7384e36cfe236f2f03c69163577e
mowadaf-education <mowadafeducation@gmail.com>
2026-05-23 12:20:52 +0000

    Deploying to gh-pages from @ mowadaf-education/LAB@86232a4081f9e540d52c1aa39dd5364e80892b6f 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/LAB/sw.js', { scope: '/LAB/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
